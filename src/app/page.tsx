8d6a3aace4a76083373f24b4afcb4074f70c15f1
Harshita Nagalli <harsheez7117@gmail.com>
2025-04-28 06:13:53 +0000

    show login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import { Bookshelf } from "@/components/bookshelf";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-8 lg:p-12">
+       {/* Development Link to Login Page */}
+       <div className="absolute top-4 right-4">
+         <Button asChild variant="outline">
+           <Link href="/login">Login</Link>
+         </Button>
+       </div>
       <Bookshelf />
     </main>
   );
